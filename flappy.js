8f5e39ae117c80af17ea04517803cd8fce30ebf7
guilhermealbm <gui.miranda.13@gmail.com>
2019-07-16 23:26:43 -0300

    adding bird movement w/ gravity and click/keydown

--- flappy.js
+++ flappy.js
@@ -1,13 +1,13 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var bird = new Image();
+var bd = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeNorth = new Image();
 var pipeSouth = new Image();
 
-bird.src = "assets/images/bird.png";
+bd.src = "assets/images/bird.png";
 bg.src = "assets/images/bg.png";
 fg.src = "assets/images/fg.png";
 pipeNorth.src = "assets/images/pipeNorth.png";
@@ -24,11 +24,21 @@ var score = 0;
 
 function draw(){
     ctx.drawImage(bg,0,0);
+    ctx.drawImage(fg,0,(canvas.height-fg.height));
+
+    var bird = ctx.drawImage(bd, bX, bY);
+    requestAnimationFrame(draw);
+
+
+    bY += gravity
+
 }
 
 document.addEventListener("keydown",moveUp);
 document.addEventListener("click",moveUp); //able to use mouse too.
 
 function moveUp(){
-    console.log("Pressed.")
+    bY -= 25;
 }
+
+draw();
