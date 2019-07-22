9cc75eb7a537f4a3203c1cab0d0bf6dd25911564
guilhermealbm <gui.miranda.13@gmail.com>
2019-07-21 23:11:48 -0300

    adding firebase script

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+(function(){
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "",
+    authDomain: "flappy-fca06.firebaseapp.com",
+    databaseURL: "https://flappy-fca06.firebaseio.com",
+    projectId: "flappy-fca06",
+    storageBucket: "",
+    messagingSenderId: "871937445505",
+    appId: "1:871937445505:web:90d101dffc7f013d"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+})()
